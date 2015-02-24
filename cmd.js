ff72abf7237ddd0b79d66f25d743d6fee3bca95e
Casper Beyer <caspervonb@qup.io>
2015-02-24 19:10:45 +0800

    Add bundler support

--- cmd.js
+++ cmd.js
@@ -1,5 +1,7 @@
 var cmd = require('commander');
 var veer = require('./');
+var temp = require('temp');
+
 var pkg = require('./package.json');
 
 cmd.usage('[options] <script>');
@@ -12,6 +14,23 @@ cmd.parse(process.argv);
 
 cmd.cwd = process.cwd();
 cmd.browser = process.env['BROWSER'];
+cmd.bundler = process.env['BUNDLER'];
+
+cmd.scripts = cmd.args.reduce(function(object, value, key) {
+  object[key] = value;
+  return object;
+}, {});
+
+if (cmd.bundler) {
+  cmd.scripts = { 'bundle.js': temp.path({suffix: '.js'}) };
+
+  cmd.args.push('-o');
+  cmd.args.push(cmd.scripts['bundle.js']);
+
+  veer.bundle(cmd, function() {
+    console.log('bundler');
+  });
+}
 
 // serve scripts and resources
 var server = veer.serve(cmd, function() {
--- index.js
+++ index.js
@@ -11,23 +11,26 @@ function serve(options, callback) {
       response.setHeader('content-type', 'text/html');
       response.write('<!doctype html><head><meta charset="utf-8"></head><body>');
 
-      for (var i = 0; i < options.args.length; i++) {
-        response.write('<script src="' + options.args[i] + '"></script>');
+      for (var key in options.scripts) {
+        response.write('<script src="' +  key + '"></script>');
       }
-      
+
       response.end('</body>');
-    }
-    else {
-      var filename = path.join(options.cwd, request.url);
+    } else {
+      var filename = options.scripts[path.basename(request.url)];
+      if (!filename) {
+        var filename = path.join(options.cwd, request.url);
+      }
+
+      console.log(filename);
 
       fs.exists(filename, function(exists) {
         if (exists) {
           response.setHeader('content-type', mime.lookup(filename));
           response.writeHead(200);
-          
+
           var file = fs.createReadStream(filename);
           file.pipe(response);
-
         } else {
           response.writeHead(404);
           response.end('404');
@@ -40,10 +43,16 @@ function serve(options, callback) {
   return server;
 }
 
+function bundle(options, callback) {
+  var cmd = util.format('%s %s', options.bundler, options.args.join(' '));
+  return child.exec(cmd, callback);
+}
+
 function browse(options, callback) {
   var cmd = util.format('%s http://%s:%d', options.browser, options.host, options.port);
   return child.exec(cmd, callback);
 }
 
 module.exports.serve = serve;
+module.exports.bundle = bundle;
 module.exports.browse = browse;
