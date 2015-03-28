stereokai/amok

commit 4aec664dbdf09bb0139890bb0b9a5f0749d70593

Keep quoted strings intact, fixes #26

index.js
@@ -44,7 +44,7 @@ function serve(options, callback) {
 }
 
 function bundle(options, callback) {
-  var args = options.bundler.split(' ');
+  var args = options.bundler.match(/\w+|"(?:\\"|[^"])+"/g);
   var cmd = args.shift();
 
   args = args.concat(options.args);
@@ -117,7 +117,7 @@ function debug(options, callback) {
 }
 
 function browse(options, callback) {
-  var args = options.browser.split(' ');
+  var args = options.browser.match(/\w+|"(?:\\"|[^"])+"/g);
   var cmd = args.shift();
 
   var url = util.format('http://%s:%d', options.host, options.port);

commit c143b0a8229b44d2c8c9d86776abd75e09a935e7

Fix options parsing regression, references #26
Fixes regression introduced by commit 4aec664dbdf09bb0139890bb0b9a5f0749d70593
In reference to issue #26

index.js
@@ -44,7 +44,7 @@ function serve(options, callback) {
 }
 
 function bundle(options, callback) {
-  var args = options.bundler.match(/\w+|"(?:\\"|[^"])+"/g);
+  var args = options.bundler.match(/\S+|"[^"]+"/g);
   var cmd = args.shift();
 
   args = args.concat(options.args);
@@ -117,7 +117,7 @@ function debug(options, callback) {
 }
 
 function browse(options, callback) {
-  var args = options.browser.match(/\w+|"(?:\\"|[^"])+"/g);
+  var args = options.browser.match(/\S+|"[^"]+"/g);
   var cmd = args.shift();
 
   var url = util.format('http://%s:%d', options.host, options.port);