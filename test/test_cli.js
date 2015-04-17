ce1b7e6c5a7d519a032502a7e377ad67f7b16cb9
Casper Beyer <caspervonb@qup.io>
2015-04-17 13:55:58 +0800

    Add browerify with babelify test case

--- test/test_cli.js
+++ test/test_cli.js
@@ -11,3 +11,13 @@ test('amok --client chrome fixture/console.js', function(t) {
     exe.kill();
   });
 });
+
+test('amok --client chrome --compiler browserify fixture/babel.js -- --transform babelify', function(t) {
+  t.plan(1);
+
+  var exe = child.spawn('node', [bin, '--client', 'chrome', '--compiler', 'browserify', 'test/fixture/babel.js', '--', '--transform', 'babelify']);
+  exe.stdout.once('data', function(data) {
+    t.equal(data.toString(), 'ok\n');
+    exe.kill();
+  });
+});
