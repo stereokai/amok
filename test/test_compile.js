de4c22759eb46b4f7478aae90332bfac0bd7f111
Casper Beyer <caspervonb@qup.io>
2015-04-23 15:36:08 +0800

    Use the entire pathname as the relative script pathname

--- test/test_compile.js
+++ test/test_compile.js
@@ -9,13 +9,18 @@ test('compile with browserify', function(t) {
 
   var exe = amok.compile('browserify', args);
   exe.on('ready', function(scripts) {
-    t.ok(scripts);
-
     var filenames = Object.keys(scripts);
     t.equal(filenames.length, 1);
 
     var stats = fs.statSync(filenames[0]);
     t.ok(stats.size > 1);
+
+    var pathnames = filenames.map(function(filename) {
+      return scripts[filename];
+    });
+
+    t.equal(pathnames.length, 1);
+    t.equal(pathnames[0], 'test/fixture/bundle.js');
   });
 
   exe.on('close', function(code, signal) {
