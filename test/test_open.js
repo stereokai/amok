d703036b31a9e094beb8b1b4caa616592e7ad090
Casper Beyer <caspervonb@qup.io>
2015-04-24 15:43:01 +0800

    Remove stale options from open test

--- test/test_open.js
+++ test/test_open.js
@@ -2,7 +2,7 @@ var amok = require('../');
 var test = require('tape');
 
 test('open chrome', function(t) {
-  var client = amok.open('chrome', ['about:blank'], options);
+  var client = amok.open('chrome', ['about:blank']);
   client.on('ready', function() {
     t.ok(client.pid);
     client.kill();
