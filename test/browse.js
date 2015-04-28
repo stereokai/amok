dd1cddc6e3b2bd24a6ef211c2eb54bcdc1e0795a
Casper Beyer <caspervonb@qup.io>
2015-04-28 21:57:55 +0800

    Fix browser iteration in browse test

--- test/browse.js
+++ test/browse.js
@@ -2,11 +2,11 @@ var amok = require('../');
 var test = require('tape');
 
 var browsers = ['chrome'];
-browsers.forEach(function(browser) {
-  test('browse about:blank with ' + browser, function(t) {
+browsers.forEach(function(name) {
+  test('browse about:blank with ' + name, function(t) {
     t.plan(1);
 
-    var exe = amok.browse('chrome', ['about:blank']);
+    var exe = amok.browse(name, ['about:blank']);
     exe.on('ready', function() {
       t.ok(exe.pid);
     });
