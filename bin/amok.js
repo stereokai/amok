b50c7fb79e3058e207bc64b7fe298294469ff6a2
Casper Beyer <caspervonb@qup.io>
2015-04-28 15:06:52 +0800

    Refactor async tasks

--- bin/amok.js
+++ bin/amok.js
@@ -12,254 +12,218 @@ var url = require('url');
 var bole = require('bole');
 var bistre = require('bistre');
 
-function program(callback, data) {
-  var cmd = require('commander');
-  var pkg = require('../package.json');
+var program = require('commander');
+var package = require('../package.json');
 
-  cmd.usage('[options] <script | url>');
-  cmd.version(pkg.version);
+program.usage('[options] <script | url>');
+program.version(package.version);
 
-  cmd.option('-s, --debugger-host <HOST>', 'specify debugger host', 'localhost');
-  cmd.option('-r, --debugger-port <PORT>', 'specify debugger port', 9222);
-  cmd.option('-b, --browser <BROWSER>', 'specify browser');
-  cmd.option('-c, --compiler <COMPILER>', 'specify compiler');
-  cmd.option('-a, --host <HOST>', 'specify http host', 'localhost');
-  cmd.option('-p, --port <PORT>', 'specify http port', 9966);
-  cmd.option('-w, --watch <GLOB>', 'specify watch pattern', null);
-  cmd.option('-i, --interactive', 'enable interactive mode');
-  cmd.option('-v, --verbose', 'enable verbose mode');
+program.option('-s, --debugger-host <HOST>', 'specify debugger host', 'localhost');
+program.option('-r, --debugger-port <PORT>', 'specify debugger port', 9222);
+program.option('-b, --browser <BROWSER>', 'specify browser');
+program.option('-c, --compiler <COMPILER>', 'specify compiler');
+program.option('-a, --host <HOST>', 'specify http host', 'localhost');
+program.option('-p, --port <PORT>', 'specify http port', 9966);
+program.option('-w, --watch <GLOB>', 'specify watch pattern', null);
+program.option('-i, --interactive', 'enable interactive mode');
+program.option('-v, --verbose', 'enable verbose mode');
 
-  cmd.parse(process.argv);
-  cmd.cwd = process.cwd();
+program.parse(process.argv);
+program.cwd = process.cwd();
 
-  if (cmd.args.length < 1) {
-    cmd.help();
-  }
-
-  var scripts = cmd.args.filter(function(arg) {
-    return arg.match(/[^\\/]+$/);
-  });
-
-  var url = cmd.args.filter(function(arg) {
-    return arg.match(/^(http|https):\/\//);
-  }).pop();
-
-  cmd.scripts = scripts.reduce(function(object, value, key) {
-    object[path.resolve(value)] = value;
-    return object;
-  }, {});
-
-  cmd.url = url;
-
-  var transform = bistre.createStream();
-  transform.pipe(process.stderr);
-
-  bole.output({
-    level: cmd.verbose ? 'debug' : 'error',
-    stream: transform
-  });
-
-  callback(null, cmd);
+if (program.args.length < 1) {
+  program.help();
 }
 
-function compiler(callback, data) {
-  var log = bole('compile');
+var scripts = program.args.filter(function(arg) {
+  return arg.match(/[^\\/]+$/);
+});
 
-  if (data.program.compiler === undefined) {
-    log.info('skip');
-    return callback(null, null);
-  }
+var url = program.args.filter(function(arg) {
+  return arg.match(/^(http|https):\/\//);
+}).pop();
 
-  log.info('spawn');
-  var compiler = amok.compile(data.program.compiler, data.program.args, data.program);
-  compiler.on('ready', function(scripts) {
-    log.info('ok', { pid: compiler.pid });
+program.scripts = scripts.reduce(function(object, value, key) {
+  object[path.resolve(value)] = value;
+  return object;
+}, {});
 
-    data.program.scripts = scripts;
+program.url = url;
 
-    callback(null, compiler);
-  });
+var transform = bistre.createStream();
+transform.pipe(process.stderr);
 
-  compiler.stdout.on('data', function(data) {
-    log.info(data.toString());
-  });
+bole.output({
+  level: program.verbose ? 'debug' : 'error',
+  stream: transform
+});
 
-  compiler.stderr.on('data', function(data) {
-    log.error(data.toString());
-  });
-
-  process.on('exit', function() {
-    compiler.kill('SIGTERM');
-  });
-}
-
-function watcher(callback, data) {
-  var log = bole('watch');
-
-  log.info('start');
-
-  var patterns = [];
-  if (data.program.watch) {
-    patterns.push(data.program.watch);
-  }
-
-  var filenames = Object.keys(data.program.scripts);
-  if (filenames.length > 0) {
-    patterns.push(path.dirname(filenames[0]));
-  }
-
-  var watcher = amok.watch(patterns);
-  watcher.on('ready', function() {
-    log.info('ok');
-    callback(null, watcher);
-  });
-
-  watcher.on('add', function(filename) {
-    log.info('add', { filename: filename });
-  });
-
-  watcher.on('unlink', function(filename) {
-    log.info('remove', { filename: filename, pathname: filename });
-  });
-
-  watcher.on('change', function(filename) {
-    log.info('change', { filename: filename });
-  });
-
-  watcher.on('error', function(error) {
-    log.error(error);
-  });
-}
-
-function server(callback, data) {
-  var log = bole('http');
-
-  if (data.program.url) {
-    log.info('skip');
-    return callback(null, null);
-  }
-
-  log.info('starting');
-
-  var server = amok.serve(data.program.port, data.program.host, data.program);
-  server.on('listening', function() {
-    var address = server.address();
-    log.info('listening', { host: address.address, port: address.port });
-
-    data.program.url = util.format('http://%s:%d', data.program.host, data.program.port);
-    callback(null, server);
-  });
-
-  server.on('request', function(request, response) {
-    log.info(request);
-  });
-
-  server.on('error', function(error) {
-    log.error(error);
-  });
-}
-
-function browser(callback, data) {
-  var log = bole('browser');
-
-  if (data.program.browser === undefined) {
-    log('skip');
-    return callback(null, null);
-  }
-
-  log.info('spawn');
-  var browser = amok.browse(data.program.browser, [data.program.url], data.program);
-  browser.on('ready', function() {
-    log.info('ok', { pid: browser.pid });
-    callback(null, browser);
-  });
-
-  browser.on('error', function(error) {
-    log.error(error);
-    process.exit(error.errno);
-  });
-
-  browser.stdout.on('data', function(data) {
-    log.info(data.toString());
-  });
-
-  browser.stderr.on('data', function(data) {
-    log.warn(data.toString());
-  });
-
-  process.on('exit', function() {
-    browser.kill('SIGTERM');
-  });
-}
-
-function bugger(callback, data) {
-  var log = bole('debugger');
-
-  log.info('connect');
-
-  data.program.watcher = data.watcher;
-  var bugger = amok.debug(data.program, function() {
-    callback(null, bugger);
-  });
-
-  bugger.on('source', function(script) {
-    log.info('source', { url: script.url });
-  });
-
-  bugger.on('attach', function(target) {
-    log.info('attach', { url: target.url, title: target.title });
-  });
-
-  bugger.on('detatch', function() {
-    log.warn('detatch');
-  });
-
-  bugger.on('error', function(error) {
-    log.error(error);
-  });
-
-  bugger.console.on('data', function(message) {
-    if (message.parameters) {
-      var parameters = message.parameters.map(function(parameter) {
-        return parameter.value;
-      });
-
-      if (console[message.level]) {
-        console[message.level].apply(console, parameters);
-      }
+async.auto({
+  compiler: function(callback, data) {
+    if (program.compiler === undefined) {
+      return callback(null, null);
     }
-  });
-}
 
-function prompt(callback, data) {
-  if (data.program.interactive === undefined) {
-    return callback(null, null);
-  }
+    var log = bole('compiler');
+    log.info('spawn');
 
-  var options = {
-    prompt: '> ',
-    input: process.stdin,
-    output: process.stdout,
-    writer: function(result) {
-      if (result.value) {
-        return util.inspect(result.value, {
-          colors: true,
+    var compiler = amok.compile(program.compiler, program.args, program);
+    compiler.on('ready', function(scripts) {
+      log.info('ok', { pid: compiler.pid });
+
+      program.scripts = scripts;
+      callback(null, compiler);
+    });
+
+    compiler.stdout.on('data', function(data) {
+      log.info(data.toString());
+    });
+
+    compiler.stderr.on('data', function(data) {
+      log.error(data.toString());
+    });
+
+    process.on('exit', function() {
+      compiler.kill('SIGTERM');
+    });
+  },
+  debugger: ['browser', 'watcher', function(callback, data) {
+    var log = bole('debugger');
+    log.info('connect');
+
+    program.watcher = data.watcher;
+    var bugger = amok.debug(program, function() {
+      callback(null, bugger);
+    });
+
+    bugger.on('source', function(script) {
+      log.info('source', { url: script.url });
+    });
+
+    bugger.on('attach', function(target) {
+      log.info('attach', { url: target.url, title: target.title });
+    });
+
+    bugger.on('detatch', function() {
+      log.warn('detatch');
+    });
+
+    bugger.on('error', function(error) {
+      log.error(error);
+    });
+
+    bugger.console.on('data', function(message) {
+      if (message.parameters) {
+        var parameters = message.parameters.map(function(parameter) {
+          return parameter.value;
         });
-      } else if (result.objectId) {
-        return util.format('[class %s]\n%s', result.className, result.description);
+
+        if (console[message.level]) {
+          console[message.level].apply(console, parameters);
+        }
       }
-    },
+    });
+  }],
 
-    eval: function(cmd, context, filename, write) {
-      data.bugger.evaluate(cmd, function(error, result) {
-        write(error, result);
-      });
-    },
-  };
+  browser: ['server', function(callback, data) {
+    var log = bole('browser');
 
-  var prompt = repl.start(options);
-  callback(null, prompt);
-}
+    if (program.browser === undefined) {
+      log('skip');
+      return callback(null, null);
+    }
+
+    log.info('spawn');
+    var browser = amok.browse(program.browser, [program.url], program);
+    browser.on('ready', function() {
+      log.info('ok', { pid: browser.pid });
+      callback(null, browser);
+    });
+
+    browser.on('error', function(error) {
+      log.error(error);
+      process.exit(error.errno);
+    });
+
+    browser.stdout.on('data', function(data) {
+      log.info(data.toString());
+    });
+
+    browser.stderr.on('data', function(data) {
+      log.warn(data.toString());
+    });
+
+    process.on('exit', function() {
+      browser.kill('SIGTERM');
+    });
+  }],
+
+  server: ['compiler', function(callback, data) {
+    var log = bole('http');
+
+    if (program.url) {
+      log.info('skip');
+      return callback(null, null);
+    }
+
+    log.info('starting');
+
+    var server = amok.serve(program.port, program.host, program);
+    server.on('listening', function() {
+      var address = server.address();
+      log.info('listening', { host: address.address, port: address.port });
+
+      program.url = util.format('http://%s:%d', program.host, program.port);
+      callback(null, server);
+    });
+
+    server.on('request', function(request, response) {
+      log.info(request);
+    });
+
+    server.on('error', function(error) {
+      log.error(error);
+    });
+  }],
+
+  watcher: ['compiler', function(callback, data) {
+    var log = bole('watch');
+
+    log.info('start');
+
+    var patterns = [];
+    if (program.watch) {
+      patterns.push(program.watch);
+    }
+
+    var filenames = Object.keys(program.scripts);
+    if (filenames.length > 0) {
+      patterns.push(path.dirname(filenames[0]));
+    }
+
+    var watcher = amok.watch(patterns);
+    watcher.on('ready', function() {
+      log.info('ok');
+      callback(null, watcher);
+    });
+
+    watcher.on('add', function(filename) {
+      log.info('add', { filename: filename });
+    });
+
+    watcher.on('unlink', function(filename) {
+      log.info('remove', { filename: filename, pathname: filename });
+    });
+
+    watcher.on('change', function(filename) {
+      log.info('change', { filename: filename });
+    });
+
+    watcher.on('error', function(error) {
+      log.error(error);
+    });
+  }],
+});
 
 process.on('SIGTERM', function() {
   process.exit(0);
@@ -267,19 +231,4 @@ process.on('SIGTERM', function() {
 
 process.on('SIGINT', function() {
   process.exit(0);
-});
-
-async.auto({
-  program: [program],
-  compiler: ['program', compiler],
-  watcher: ['program', 'compiler', watcher],
-  server: ['program', 'compiler', server],
-  browser: ['program', 'server', browser],
-  bugger: ['program', 'browser', 'watcher', bugger],
-  prompt: ['program', 'bugger', prompt]
-}, function(error, data) {
-  if (error) {
-    console.error(error);
-    process.exit(1);
-  }
 });
