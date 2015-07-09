a223ac8c09331fd45dd64510c527201c3da3b9ed
Casper Beyer <caspervonb@qup.io>
2015-07-10 05:37:59 +0800

    Extract hotpatch compiler tests into seperate files

---
+++ test/bin_hotpatch_compiler.js
@@ -0,0 +1,92 @@
+var child = require('child_process');
+var http = require('http');
+var test = require('tape');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+
+var browsers = [
+  'chrome',
+  'chromium',
+];
+
+var compilers = [
+  'babel',
+  'coffee',
+  'tsc',
+  'watchify',
+  'webpack',
+];
+
+browsers.forEach(function (browser) {
+  compilers.forEach(function (compiler, index) {
+    test('bin hot patch basic compiled with ' + compiler + ' in ' + browser, function (test) {
+      test.plan(13);
+
+      var dirname = 'test/fixture/hotpatch-' + compiler;
+      var entries = fs.readdirSync(dirname).map(function (filename) {
+        return path.join(dirname, filename);
+      }).filter(function (filename) {
+        return filename.match(/(.js|.ts|.coffee)$/);
+      });
+
+      var args = [
+        'bin/amok.js',
+        '--port',
+        9966 + index,
+        '--hot',
+        '--compiler',
+        compiler,
+        '--browser',
+        browser,
+        entries[0]
+      ];
+
+      test.comment(args.join(' '));
+
+      var cli = child.spawn('node', args);
+      cli.stderr.pipe(process.stderr);
+
+      cli.on('close', function () {
+        test.pass('close');
+      });
+
+      var values = [
+        'ready',
+        'step-0',
+        'step-1',
+        'step-2',
+        'step-3',
+        'step-4',
+        'step-5',
+        'step-6',
+        'step-7',
+        'step-8',
+        'step-9',
+        'step-0',
+      ];
+
+      var source = fs.readFileSync(entries[0], 'utf-8');
+      cli.stdout.setEncoding('utf-8');
+      cli.stdout.on('data', function (chunk) {
+        chunk.split('\n').forEach(function (line) {
+          if (line === '') {
+            return;
+          }
+
+          test.equal(line, values.shift(), line);
+
+          if (values[0] === undefined) {
+            cli.kill('SIGTERM')
+          } else if (line.match(/step/)) {
+            source = source.replace(line, values[0]);
+
+            setTimeout(function () {
+              fs.writeFileSync(entries[0], source, 'utf-8');
+            }, 1000);
+          }
+        });
+      });
+    });
+  });
+});
---
+++ test/lib_hotpatch_compiler.js
@@ -0,0 +1,87 @@
+var amok = require('..');
+var test = require('tape');
+var fs = require('fs');
+var path = require('path');
+
+var browsers = [
+  'chrome',
+  'chromium',
+];
+
+browsers.forEach(function (browser, index) {
+  var compilers = [
+    'babel',
+    'coffee',
+    'tsc',
+    'watchify',
+    'webpack',
+  ];
+
+  compilers.forEach(function (compiler) {
+    test('hot patch basic script compiled with ' + compiler + ' in ' + browser, function (test) {
+      test.plan(35);
+
+      var dirname = 'test/fixture/hotpatch-' + compiler;
+      var entries = fs.readdirSync(dirname).map(function (filename) {
+        return path.join(dirname, filename);
+      }).filter(function (filename) {
+        return filename.match(/(.js|.ts|.coffee)$/);
+      });
+
+      var runner = amok.createRunner();
+      runner.on('close', function () {
+        test.pass('close');
+      });
+
+      runner.set('port', 4000 + index);
+
+      runner.use(amok.server(9966, 'localhost'));
+      runner.use(amok.compiler(compiler, entries, process.stderr));
+
+      runner.use(amok.browser(browser));
+      runner.use(amok.hotpatch());
+
+      runner.connect(runner.get('port'), 'localhost', function () {
+        test.pass('connect');
+
+        var values = [
+          'ready',
+          'step-0',
+          'step-1',
+          'step-2',
+          'step-3',
+          'step-4',
+          'step-5',
+          'step-6',
+          'step-7',
+          'step-8',
+          'step-9',
+          'step-0',
+        ];
+
+        var source = fs.readFileSync(entries[0], 'utf-8');
+
+        runner.client.console.on('data', function (message) {
+          test.equal(message.text, values.shift(), message.text);
+
+          if (values[0] === undefined) {
+            runner.close();
+          } else if (message.text.match(/step/)) {
+            source = source.replace(message.text, values[0]);
+            test.notEqual(source, fs.readFileSync(entries[0]));
+
+            setTimeout(function () {
+              fs.writeFile(entries[0], source, 'utf-8', function (error) {
+                test.error(error);
+              });
+            }, 1000);
+          }
+        });
+
+        runner.client.console.enable(function (error) {
+          test.error(error);
+        });
+      });
+    });
+  });
+});
